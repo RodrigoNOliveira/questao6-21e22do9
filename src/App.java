8b8ba8dfc0c68f55ace07a0b040ae2b6523f2f63
Rodrigo Nathan Oliveira <rodrigonathan4101@gmail.com>
2021-11-03 11:38:28 -0300

    Codigo criado

---
+++ src/App.java
@@ -0,0 +1,13 @@
+class App {
+    public static void main(String[] args) {
+      System.out.println("Contagem regressiva");
+    
+    int numero= 10;
+    for (int i = numero; i >= 0; i--){        
+        System.out.println(i);
+        }
+  
+      System.out.println("Fim");
+  }
+  
+  }
